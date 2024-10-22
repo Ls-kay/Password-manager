cc3430ee7737c3b0114544c198d8e1b4c3ee6e64
Lebohang M <msaselebohang@gmail.com>
2024-10-22 07:18:27 +0200

    Fixed the module error and tested the program

--- manager.py
+++ manager.py
@@ -8,7 +8,7 @@ KEY_FILE = 'key.key'
 
 
 def Generate_Key():
-    key = Fernet.Generate_Key()
+    key = Fernet.generate_key()
     with open(KEY_FILE, 'wb') as key_file:
         key_file.write(key)
 
@@ -59,5 +59,41 @@ def retrieve_password(account, key):
         print(f"Password not found for: {account}")
         return None
 
+
 def main():
-    
+    if not os.path.exists(KEY_FILE):
+        print("Encryption not found. Generating a new key...")
+        Generate_Key()
+
+    key = Load_Key()
+
+    while True:
+        print("--- Password Manager ---")
+        print("1. Add a new password")
+        print("2. Retrieve a password")
+        print("3. Exit")
+
+        option = input("Enter your option: ")
+
+        if option == "1":
+            account = input("Enter account name (e.g., website or app name): ")
+            password = getpass("Enter the password: ")
+            Add_Password(account, password, key)
+
+        elif option == "2":
+            account = input("Enter account name: ")
+            password = retrieve_password(account, key)
+
+            if password:
+                print(f"Password for {account}: {password}")
+
+        elif option == "3":
+            print("Exiting the Password Manager.")
+            break
+
+        else:
+            account = input("Invalid choice. Enter correct choice: ")
+
+
+if __name__ == "__main__":
+    main()
